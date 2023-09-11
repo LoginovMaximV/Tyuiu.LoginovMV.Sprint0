50c3883944b7008c590e9d7c092ff70f6c423b92
Tyuiu.LoginovMV@outlook.com <maximloginov22@gmail.com>
2023-09-11 22:51:15 +0500

    Task6

---
+++ Tyuiu.LoginovMV.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LoginovMV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAddArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AddArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+
+        public void CheckSubtrArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtrArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+
+        public void CheckMultiplicationArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
