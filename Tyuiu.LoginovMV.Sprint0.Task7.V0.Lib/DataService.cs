e623e321cfa529b090bc37d5daaebdd6cc3a315e
Tyuiu.LoginovMV@outlook.com <maximloginov22@gmail.com>
2023-09-13 18:08:05 +0500

    Taslk7

---
+++ Tyuiu.LoginovMV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tyuiu.LoginovMV.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] a, int[] b)
+        {
+            int[] resultArrays = new int[5];
+            for (var i = 0;i<resultArrays.Length;i++)
+            {
+                resultArrays[i] = a[i] + b[i];
+            }
+            return resultArrays;
+        }
+    }
+}
---
+++ Tyuiu.LoginovMV.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LoginovMV.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            int[] numbers1 = new int[] { 1, 1, 1, 1, 1 };
+            int[] numbers2 = new int[] { 1, 1, 1, 1, 1 };
+            int[] numbers3 = new int[] { 2, 2, 2, 2, 2 };
+            int[] res = DataService.AdditionArrays(numbers1, numbers2);
+            CollectionAssert.AreEqual(numbers3, res);
+        }
+    }
+}
